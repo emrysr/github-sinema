c13b5a2647d169b85e003b08a3b3fcd6b1c6b849
Emrys <emrys.r@gmail.com>
2023-01-17 20:22:28 +0000

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,7 +6,7 @@
       Vite Documentation
     </a>
     |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+    <a href="https://vercel.com" target="_blank">Deployed on vercel.com</a>
   </p>
 
   <button type="button" @click="state.count++">count is: {{ state.count }}</button>
